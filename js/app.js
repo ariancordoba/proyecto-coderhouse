30fd22a0320f5cedc7e61bde3f8ebf0abab30da3
Arian Cordoba <arian.gasolero@gmail.com>
2022-09-13 01:26:30 -0300

    Agregando Scroll Reveal

---
+++ js/app.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "-100px",
+});
+
+sr.reveal(".hero", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "-100px",
+});
+
+sr.reveal(".grid", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "-100px",
+});
+
+sr.reveal(".events", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "-200px",
+});
+
+sr.reveal(".container__footer", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "100px",
+});
